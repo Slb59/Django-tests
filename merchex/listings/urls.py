47eca33306cfd0f676ab98dc75eb954d80b7809a
Slb59 <sylvie.bricout2@laposte.net>
2023-04-27 20:31:35 +0200

    Add contact page

--- merchex/listings/urls.py
+++ merchex/listings/urls.py
@@ -5,6 +5,9 @@ app_name = "listings"
 urlpatterns = [
     path("bands/", views.band_list, name='band-list'),
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
-    path("listings/", views.listings),
-    path("about_us/", views.about),
+    path('bands/add/', views.band_create, name='band-create'),
+    path("listings/", views.listings, name='listings'),
+    path("about_us/", views.about, name='about_us'),
+    path('contact-us/', views.contact, name='contact'),
+    path('email-sent/', views.email_sent, name='email-sent'),
 ]
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,9 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 from listings.models import Band, Listing
+from listings.forms import ContactUsForm, BandForm
 
 
 def band_list(request):
@@ -26,3 +29,51 @@ def listings(request):
 
 def about(request):
     return render(request, 'listings/about.html')
+
+
+def contact(request):    
+
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)        
+
+        if form.is_valid():
+            subject = 'Message from '
+            subject += f'{form.cleaned_data["name"] or "anonyme"} '
+            subject += 'via MerchEx Contact Us form'
+            send_mail(
+                subject=subject,
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+        return redirect('listings:email-sent') 
+    else:
+        form = ContactUsForm()  # ajout d’un nouveau formulaire ici
+
+    return render(request,
+                  'listings/contact.html',
+                  {'form': form})  # passe ce formulaire au gabarit
+
+
+def email_sent(request):
+    return render(request, 'listings/email_sent.html')
+
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            # créer une nouvelle « Band » et la sauvegarder dans la db
+            band = form.save()
+            # redirige vers la page de détail du groupe
+            # que nous venons de créer
+            # nous pouvons fournir les arguments du motif url comme arguments
+            # à la fonction de redirection
+            return redirect('listings:band-detail', band.id)
+
+    else:
+        form = BandForm()
+
+    return render(request,
+                  'listings/band_create.html',
+                  {'form': form})
