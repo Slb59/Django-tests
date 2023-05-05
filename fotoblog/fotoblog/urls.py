3f3eb3ba88c62cb31a40e7ede0812ceb1ec45793
Slb59 <sylvie.bricout2@laposte.net>
2023-05-05 13:09:36 +0200

    add password change systeme

--- fotoblog/fotoblog/urls.py
+++ fotoblog/fotoblog/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView
 
 import authentication.views
 import blog.views
@@ -29,6 +29,12 @@ urlpatterns = [
         template_name='authentication/login.html',
         redirect_authenticated_user=True),
         name='login'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change_form.html'),
+        name='password_change'),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+        name='password_change_done'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
     path('signup/', authentication.views.signup_page, name='signup'),
