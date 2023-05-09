0679a0107262318fe128a980e43612026159a9ed
Slb59 <sylvie.bricout2@laposte.net>
2023-05-09 08:55:06 +0200

    add static file

--- fotoblog/fotoblog/settings.py
+++ fotoblog/fotoblog/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -120,6 +121,7 @@ AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = LOGIN_URL
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
