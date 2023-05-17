b3439e7234173e09b0a76f36775f88aad0d9701c
Slb59 <sylvie.bricout2@laposte.net>
2023-05-17 12:18:47 +0200

    add polls app

--- tutodjango/config/urls.py
+++ tutodjango/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('tutodjango.urls', namespace='tutodjango')),
+    path('tuto/', include('tutodjango.urls', namespace='tutodjango')),
+    path('', include('polls.urls', namespace='polls')),
 ]
