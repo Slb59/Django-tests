4334c1c32c7959d0e43c774a90ddc1ccbcd76b79
Slb59 <sylvie.bricout2@laposte.net>
2023-05-22 09:45:55 +0200

    modify admin Question

--- tutodjango/polls/admin.py
+++ tutodjango/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question)
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
