468f336718881cb48e0ce95c217c2b9a18ce686f
Slb59 <sylvie.bricout2@laposte.net>
2023-05-22 09:31:32 +0200

    add statics files

--- tutodjango/config/settings.py
+++ tutodjango/config/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "tutodjango",
-    "polls",
+    "tutodjango.apps.TutodjangoConfig",
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
--- tutodjango/polls/views.py
+++ tutodjango/polls/views.py
@@ -15,8 +15,11 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         # return Question.objects.order_by('-pub_date')[:5]
-        return Question.objects.filter(
+        value = Question.objects.filter(
             pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        print(value)
+        return value
+
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
